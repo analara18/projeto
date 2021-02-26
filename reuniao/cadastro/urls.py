e051f96700ba0236ebd4f6b9d3c50b1760645a34
unknown <analara.al375@gmail.com>
2021-02-26 00:12:16 -0300

    adicionou cadastro/urls, cadastro/views, reuniao/urls

--- reuniao/cadastro/urls.py
+++ reuniao/cadastro/urls.py
@@ -5,9 +5,20 @@ from cadastro import views
 
 urlpatterns = [  
     path('admin/', admin.site.urls),  
+	path('', views.show_menu),  
     path('cadastro-pessoa', views.cadastro_pessoa),  
     path('show-pessoa',views.show_pessoa),  
     path('edit-pessoa/<int:id>', views.edit_pessoa),  
     path('update-pessoa/<int:id>', views.update_pessoa),  
     path('delete-pessoa/<int:id>', views.destroy_pessoa),  
-]  
+	path('cadastro-sala', views.cadastro_sala),  
+    path('show-sala',views.show_sala),  
+    path('edit-sala/<int:id>', views.edit_sala),  
+    path('update-sala/<int:id>', views.update_sala),  
+    path('delete-sala/<int:id>', views.destroy_sala),  
+    path('cadastro-espaco', views.cadastro_espaco),  
+    path('show-espaco',views.show_espaco),  
+    path('edit-espaco/<int:id>', views.edit_espaco),  
+    path('update-espaco/<int:id>', views.update_espaco),  
+    path('delete-espaco/<int:id>', views.destroy_espaco), 
+]
--- reuniao/cadastro/views.py
+++ reuniao/cadastro/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect  
 
-from cadastro.forms import PessoaForm  
-from cadastro.models import Pessoa
+from cadastro.forms import PessoaForm, SalaForm, EspacoForm  
+from cadastro.models import Pessoa, Sala, Espaco
 
-  
+
+# Menu do sistema FIXME TODO
+def show_menu(request):  
+    return render(request,'index_pessoa.html',{'form':form})
+
+
+# Cadastro de Pessoa
 def cadastro_pessoa(request):  
     if request.method == "POST":  
         form = PessoaForm(request.POST) 
@@ -45,4 +51,94 @@ def destroy_pessoa(request, id):
     pessoa = Pessoa.objects.get(id_pessoa=id)  
     pessoa.delete()  
     return redirect("/cadastro/show-pessoa")  
+
+
+# Cadastro de sala
+def cadastro_sala(request):  
+    if request.method == "POST":  
+        form = SalaForm(request.POST) 
+ 
+        if form.is_valid(): 
+			# Tratamento de exceção: Não retornar erro na tela  
+            try:  
+                form.save()  
+                return redirect('/cadastro/show-sala')  
+            except:  
+                pass  
+    else:  
+        form = SalaForm()  
+
+    return render(request,'index_sala.html',{'form':form})
+
+
+def show_sala(request):  
+    sala = Sala.objects.all()  
+    return render(request,"show_sala.html",{'sala': sala}) 
+
+
+def edit_sala(request, id):  
+    sala = Sala.objects.get(id_sala=id)  
+    return render(request,'edit_sala.html', {'sala': sala})  
+
+
+def update_sala(request, id):  
+    sala = Sala.objects.get(id_sala=id)  
+
+    form = SalaForm(request.POST, instance=sala)  
+    if form.is_valid():  
+        form.save()  
+        return redirect("/cadastro/show-sala")  
+    return render(request, 'edit_sala.html', {'sala': sala})  
+
+
+def destroy_sala(request, id):  
+    sala = Sala.objects.get(id_sala=id)  
+    sala.delete()  
+    return redirect("/cadastro/show-sala") 
+
+
+# cadastro de lotação
+def cadastro_espaco(request):  
+    if request.method == "POST":  
+        form = EspacoForm(request.POST) 
+ 
+        if form.is_valid(): 
+			# Tratamento de exceção: Não retornar erro na tela  
+            try:  
+                form.save()  
+                return redirect('/cadastro/show-espaco')  
+            except:  
+                pass  
+    else:  
+        form = EspacoForm()  
+
+    return render(request,'index_espaco.html',{'form':form})
+
+
+def show_espaco(request):  
+    espaco = Espaco.objects.all()  
+    return render(request,"show_espaco.html",{'espaco': espaco}) 
+
+
+def edit_espaco(request, id):  
+    espaco = Espaco.objects.get(id_espaco=id)  
+    return render(request,'edit_espaco.html', {'espaco': espaco})  
+
+
+def update_espaco(request, id):  
+    espaco = Espaco.objects.get(id_espaco=id)  
+
+    form = EspacoForm(request.POST, instance=espaco)  
+    if form.is_valid():  
+        form.save()  
+        return redirect("/cadastro/show-espaco")  
+    return render(request, 'edit_espaco.html', {'espaco': espaco})  
+
+
+def destroy_espaco(request, id):  
+    espaco = Espaco.objects.get(id_espaco=id)  
+    espaco.delete()  
+    return redirect("/cadastro/show-espaco") 
+
+
 
