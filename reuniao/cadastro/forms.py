2b5c4991213cd3e00c61356eebf8dc7449948e06
unknown <analara.al375@gmail.com>
2021-02-26 00:03:50 -0300

    Adicionado models e form

--- reuniao/cadastro/forms.py
+++ reuniao/cadastro/forms.py
@@ -1,8 +1,19 @@
 from django import forms
 
-from cadastro.models import Pessoa
+from cadastro.models import Pessoa, Sala, Espaco
 
 class PessoaForm(forms.ModelForm):
 	class Meta:
 		model = Pessoa
 		fields = '__all__'
+
+class SalaForm(forms.ModelForm):
+	class Meta:
+		model = Sala
+		fields = '__all__'
+
+class EspacoForm(forms.ModelForm):
+	class Meta:
+		model = Espaco
+		fields = '__all__'
+
--- reuniao/cadastro/models.py
+++ reuniao/cadastro/models.py
@@ -9,5 +9,23 @@ class Pessoa(models.Model):
 		db_table = "pessoa"
 
 
+class Sala(models.Model):
+	id_sala = models.AutoField(primary_key=True)
+	nome = models.CharField(max_length=100)
+	lotacao = models.IntegerField()
+
+	class Meta:
+		db_table = "sala"
+
+
+class Espaco(models.Model):
+	id_espaco = models.AutoField(primary_key=True)
+	nome = models.CharField(max_length=100)
+	lotacao = models.IntegerField()
+	
+	class Meta:
+		db_table = "espaco"
+
+
 
 
